d1b2dfd3b596ff3dfb8d28b86aefd0d784bcb585
Aaron Meurer <asmeurer@gmail.com>
2023-05-17 14:02:20 -0600

    Update scikit_learn_plot.py

--- scikit_learn_plot.py
+++ scikit_learn_plot.py
@@ -5,12 +5,14 @@ import seaborn as sns
 results = pd.read_csv("scikit_learn_timings.csv")
 
 sns.set_theme(context="paper", font_scale=1.4)
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), constrained_layout=True)
-sns.barplot(x="backend", y="duration", data=results[results["method"] == "fit"], ax=ax1)
-ax1.set_ylabel("duration (sec)")
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), constrained_layout=True, sharey=True)
+sns.barplot(y="backend", x="duration", data=results[results["method"] == "fit"], ax=ax1)
+ax1.set_xlabel("duration (sec)")
 ax1.set_title("fit")
 
-sns.barplot(x="backend", y="duration", data=results[results["method"] == "predict"], ax=ax2)
-ax2.set_ylabel("duration (sec)")
+sns.barplot(y="backend", x="duration", data=results[results["method"] == "predict"], ax=ax2)
+ax2.set_xlabel("duration (sec)")
 ax2.set_title("predict")
+
+fig.suptitle("LinearDiscriminantAnalysis")
 fig.savefig("scikit_learn_timings.pdf")
