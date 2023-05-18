ebb1ddd7c79f8718a5ce6c519af4f655a46abbb7
Aaron Meurer <asmeurer@gmail.com>
2023-05-18 16:57:56 -0600

    Add a SciPy plot and update the scikit-learn plot for consistency

--- scikit_learn_plot.py
+++ scikit_learn_plot.py
@@ -5,22 +5,38 @@ import seaborn as sns
 results = pd.read_csv("scikit_learn_timings.csv")
 
 sns.set_theme(context="paper", font_scale=1.4)
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), constrained_layout=True, sharey=True)
-sns.barplot(y="backend", x="duration", data=results[results["method"] == "fit"], ax=ax1)
-ax1.set_xlabel("duration (sec)")
-ax1.set_title("fit")
+# Keep the y-axis log ticks
+sns.set(rc={"ytick.left" : True})
 
-sns.barplot(y="backend", x="duration", data=results[results["method"] == "predict"], ax=ax2)
-ax2.set_ylabel("")
-ax2.set_xlabel("duration (sec)")
-ax2.set_title("predict")
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), constrained_layout=True)
+sns.barplot(y="Duration", x="Backend", data=results[results["Method"] == "fit"], ax=ax1, log=True)
+ax1.set_title("fit()")
 
-fig.suptitle("LinearDiscriminantAnalysis")
+sns.barplot(y="Duration", x="Backend", data=results[results["Method"] == "predict"], ax=ax2, log=True)
+ax2.set_title("predict()")
+
+for ax in ax1, ax2:
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    ax.set_xticklabels([label.get_text().replace(' ', '\n') for label in ax.get_xticklabels()])
+
+ax1.set_yticks([0.1, 1, 10])
+ax1.get_yaxis().set_major_formatter(plt.ScalarFormatter())
+ax2.set_yticks([0.01, 0.1])
+ax2.get_yaxis().set_major_formatter(plt.ScalarFormatter())
+
+# Add X-axis label to the whole plot
+fig.supxlabel("Backend")
+fig.supylabel("Duration (sec)")
+
+fig.suptitle("scikit-learn LinearDiscriminantAnalysis performance with array API backends")
+
+plt.tight_layout()
 fig.savefig("scikit_learn_timings.pdf")
 
-means = results.groupby(["backend", "method"]).mean()
+means = results.groupby(["Backend", "Method"]).mean()
 print("Mean durations:")
 print(means)
 print()
 print("Speedup over NumPy:")
-print(means.loc["numpy"] / means)
+print(means.loc["NumPy"] / means)
